d0d5c433381a48fc56a8967ebed35246d006fc24
MargaretVanessa <margaretvmolinau@gmail.com>
2023-10-23 15:43:58 -0300

    integracion con API

--- script/app.js
+++ script/app.js
@@ -48,3 +48,65 @@ document.addEventListener('DOMContentLoaded', function() {
         console.log(imagenData); // Muestra el objeto JSON en la consola
     });
 });
+
+
+// Función query
+async function query(data) {
+    const response = await fetch(
+        "https://api-inference.huggingface.co/models/stabilityai/stable-diffusion-xl-base-1.0",
+        {
+            headers: { Authorization: "Bearer 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" },
+            method: "POST",
+            body: JSON.stringify(data),
+        }
+        );
+        const result = await response.blob();
+        return result;
+    }
+    
+// Evento click para el botón "Crear imagen"
+const crearImagenBtn = document.getElementById('crearImagenBtn');
+crearImagenBtn.addEventListener('click', async () => {
+    const queQuieresVer = document.getElementById('queQuieresVer').value;
+    const opciones = document.getElementById('opciones').value;
+
+    if (!queQuieresVer) {
+        alert('Debes ingresar una descripción');
+        return;
+    }
+
+    if (opciones === 'Estilo') {
+        alert('Selecciona una opción válida');
+        return;
+    }
+
+    const imagenData = {
+        "inputs": `Descripción: ${queQuieresVer}, Opción: ${opciones}`
+    };
+
+    try {
+        const response = await query(imagenData);
+
+        // Comprueba si la respuesta es una imagen
+        if (response instanceof Blob) {
+            const imageUrl = URL.createObjectURL(response);
+
+            // Crea o actualiza un elemento <img> en el DOM
+            const imagenElement = document.getElementById('imagen');
+            if (imagenElement) {
+                imagenElement.src = imageUrl;
+                imagenElement.alt = 'Imagen generada';
+            } else {
+                const nuevaImagen = document.createElement('img');
+                nuevaImagen.id = 'imagen';
+                nuevaImagen.src = imageUrl;
+                nuevaImagen.alt = 'Imagen generada';
+                document.querySelector('.contenido').appendChild(nuevaImagen);
+            }
+        } else {
+            console.error('La respuesta no es una imagen válida.');
+        }
+    } catch (error) {
+        console.error('Error al obtener la imagen:', error);
+    }
+});
